4282472df4786c1401f6be8b74296ba7f896ceb7
elias <eliasyusti@gmail.com>
2019-10-23 19:22:29 -0400

    version rect 1.0.0

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+function Helloworld(props){
+  return(
+    <div id ="hello">
+    <h2>{props.subtitulo}</h2>
+    {props.mitexto}
+    </div>  );
+}
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>este es mi componente: 
+      <Helloworld mitexto="hola elias" subtitulo="yusti"/>
+      <Helloworld mitexto="hola pablo" subtitulo="puerta"/></div>
   );
 }
 
