476612c15fc24baaa4f9984b73d12d484292f4a3
elias <eliasyusti@gmail.com>
2019-11-15 20:30:04 -0400

    creacion de componentes

---
+++ src/components/CrearFruta.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class CrearFruta extends Component {
+    render() {
+        return (
+            <div>
+                crear fruta
+            </div>
+        )
+    }
+}
---
+++ src/components/ListaFrutas.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class ListaFrutas extends Component {
+    render() {
+        return (
+            <div>
+                lista de frutas
+            </div>
+        )
+    }
+}
