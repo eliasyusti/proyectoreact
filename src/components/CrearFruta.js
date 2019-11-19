a81cc0527f0ab38b10f9d01dd14f07965c6b4844
elias <eliasyusti@gmail.com>
2019-11-18 21:10:49 -0400

    obtencion de datos

--- src/components/CrearFruta.js
+++ src/components/CrearFruta.js
@@ -1,10 +1,96 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 
 export default class CrearFruta extends Component {
+
+    state= {
+
+        nombre:"",
+        descripcion:""
+    }
+
+
+    async componentDidMount() {
+
+        const res = await axios.get('http://localhost:3000/api/frutas');
+        console.log(res.data)
+        
+
+    }
+    //crear una nueva fruta
+
+
+    onSubmit = (e) => {
+        console.log(this.setState.nombre, this.setState.descripcion)
+        e.preventDefault();
+        
+
+        const nuevaFruta = {
+
+            nombre: this.state.nombre,
+            descripcion: this.state.descripcion
+        }
+        console.log(nuevaFruta)
+    } 
+
+    onInputChange = e => {
+        e.preventDefault();
+        this.setState({
+
+        [e.target.name]: e.target.value
+
+        })
+        
+
+    } 
+
     render() {
         return (
-            <div>
-                crear fruta
+            <div className="row">
+
+                <div className="col-md-6 offset-md-3">
+
+                    <div className="card card-body">
+                        <h4>Crear Fruta</h4>
+
+                        {/** crear nombre de la fruta*/}
+
+                        <div className="form-group">
+                            <input
+                                type="text" className="form-control"
+                                placeholder="Nombre"
+                                name="nombre"
+                                onChange={this.onInputChange}
+                                required>
+
+                            </input>
+                        </div>
+                        <div>
+                            <textarea
+                                name="descripcion"
+                                className="form-control"
+                                placeholder="Descripcion"
+                                onChange={this.onInputChange}
+                                required>
+
+                            </textarea>
+                        </div>
+                        <div>
+                        <form onSubmit={this.onSubmit}>
+
+                            <button type="submit" className="btn btn-primary">
+
+                                Guardar
+
+                            </button>
+
+                        </form>
+                        </div>
+
+                    </div>
+
+
+                </div>
             </div>
         )
     }
